db18e2eb3a3fe388593ee61c4d048df0a2c623b7
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2024-12-16 14:54:09 +0530

    complete day2 part1

---
+++ 2/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	input_file, err := os.ReadFile("input.txt")
+	check(err)
+	input_lines := strings.Split(string(input_file), "\n")
+	var input [][]int
+	for _, line := range input_lines {
+		line := strings.Split(line, " ")
+		var convertedLine []int
+		for _, element := range line {
+			converted_element, _ := strconv.Atoi(element)
+			convertedLine = append(convertedLine, converted_element)
+		}
+		input = append(input, convertedLine)
+	}
+
+	fmt.Printf("Part1 Ans: %d\n", part1(input))
+}
+
+func part1(input [][]int) int {
+	count := 0
+	for _, list := range input {
+		if isCorrectlyOrdered(list) {
+			count++
+		}
+	}
+	return count
+}
+
+func isCorrectlyOrdered(list []int) bool {
+
+	var increasingList bool
+	if list[0] > list[1] {
+		increasingList = false
+	} else {
+		increasingList = true
+	}
+
+	for i := 0; i < len(list)-1; i++ {
+		if increasingList {
+			if !(list[i] < list[i+1] && list[i+1]-list[i] <= 3 && list[i+1]-list[i] >= 1) {
+				return false
+			}
+		} else {
+			if !(list[i] > list[i+1] && list[i]-list[i+1] <= 3 && list[i]-list[i+1] >= 1) {
+				return false
+			}
+		}
+	}
+	return true
+}
