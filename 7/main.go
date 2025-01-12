0088cdd7043e062e20d31ac99a442c369d0062ba
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2025-01-12 11:53:18 +0530

    complete day7

--- 7/main.go
+++ 7/main.go
@@ -2,57 +2,76 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func parse(string_values []string) []int {
+	var values []int
+	for _, string_value := range string_values {
+		value, _ := strconv.Atoi(string_value)
+		values = append(values, value)
 	}
+	return values
 }
 
 func main() {
-	// fmt.Println("Prasanna Ezhilmurugan")
-	input_file, err := os.ReadFile("sample_input.txt")
-	check(err)
+	input_file, _ := os.ReadFile("input.txt")
 	input := strings.Split(string(input_file), "\n")
-	// for _, line := range input {
-	// 	fmt.Println(line)
-	// }
-	hm := make(map[int][]int)
-	for _, line := range input {
-		result, _ := strconv.Atoi(strings.Split(line, ": ")[0])
-
-		var operands []int
-		for _, str := range strings.Split(strings.Split(line, ": ")[1], " ") {
-			operand, _ := strconv.Atoi(str)
-			operands = append(operands, operand)
-		}
-
-		hm[result] = operands
-	}
-
-	totalCalibrationResult := 0
-	for result, operands := range hm {
-		if isCalibratable(result, operands[1:], operands[0]) {
-			totalCalibrationResult += result
-			// fmt.Println(totalCalibrationResult)
-		}
-		fmt.Println()
-	}
-	fmt.Printf("Part1 Ans: %d\n", totalCalibrationResult)
+	fmt.Printf("Part1 Ans: %d\n", part1(input))
+	fmt.Printf("Part2 Ans: %d\n", part2(input))
 }
 
-func isCalibratable(result int, operands []int, current int) bool {
-	if len(operands) == 0 {
-		if result != current {
-			return false
-		} else {
-			return true
+func part1(input []string) int {
+	result := 0
+	for _, line := range input {
+		target, _ := strconv.Atoi(strings.Split(line, ": ")[0])
+		values := parse(strings.Split(strings.Split(line, ": ")[1], " "))
+
+		if is_calibratable(target, values[1:], values[0]) {
+			result += target
 		}
 	}
-	fmt.Printf("%d | %d |\n\t\t\t%d (x|+) %d = %d\n", result, operands, current, operands[0], current*operands[0])
-	return isCalibratable(result, operands[1:], current*operands[0]) || isCalibratable(result, operands[1:], current+operands[0])
+	return result
+}
+
+func is_calibratable(target int, values []int, current int) bool {
+	if len(values) == 1 {
+		if values[0]*current == target || values[0]+current == target {
+			return true
+		} else {
+			return false
+		}
+	}
+	return is_calibratable(target, values[1:], current*values[0]) || is_calibratable(target, values[1:], current+values[0])
+}
+
+func part2(input []string) int {
+	result := 0
+	for _, line := range input {
+		target, _ := strconv.Atoi(strings.Split(line, ": ")[0])
+		values := parse(strings.Split(strings.Split(line, ": ")[1], " "))
+
+		if is_concat_calibratable(target, values[1:], values[0]) {
+			result += target
+		}
+	}
+	return result
+}
+
+func concat(operand1 int, operand2 int) int {
+	return operand1*int(math.Pow(10, float64(len(strconv.Itoa(operand2))))) + operand2
+}
+func is_concat_calibratable(target int, values []int, current int) bool {
+
+	if len(values) == 1 {
+		if values[0]*current == target || values[0]+current == target || concat(current, values[0]) == target {
+			return true
+		} else {
+			return false
+		}
+	}
+	return is_concat_calibratable(target, values[1:], concat(current, values[0])) || is_concat_calibratable(target, values[1:], current*values[0]) || is_concat_calibratable(target, values[1:], current+values[0])
 }
